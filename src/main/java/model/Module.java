370ce59cae56b4152cf7ddc9e04c618006bd394d
cardosofelipe <gasparfelipe.cardosodesantana@student.supsi.ch>
2016-03-03 12:12:55 +0100

    Module Addes

---
+++ src/main/java/model/Module.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by felipe on 03/03/16.
+ */
+public class Module {
+    private String id;
+    private String name;
+    private String description;
+    private String type;
+    private ArrayList<String> cparams;
+    //private Map<String, Value> parameters;
+
+
+}
