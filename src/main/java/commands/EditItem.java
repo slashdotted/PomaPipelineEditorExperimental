c9247c52852219dbfccfee44032acdf8e3ef97e5
MarcoDiazLopez <marco.diazlopez@student.supsi.ch>
2016-04-30 17:32:37 +0200

    EditItem command

--- src/main/java/commands/EditItem.java
+++ src/main/java/commands/EditItem.java
@@ -3,28 +3,30 @@ package commands;
 import controller.MainWindow;
 import controller.SideBar;
 import main.Main;
+import model.Link;
 import model.Module;
 
 /**
  * Created by felipe on 28/04/16.
  */
 public class EditItem<T> implements Command {
-
+    T oldestValue;
     T oldValue;
     T newValue;
     Module module;
 
     public EditItem(T oldValue, T newValue) {
         this.oldValue = oldValue;
+        this.oldestValue=oldValue;
         this.newValue = newValue;
         this.module = null;
     }
 
-    public EditItem(T oldValue, T newValue, Module module) {
+ /*   public EditItem(T oldValue, T newValue, Module module) {
         this.oldValue = oldValue;
         this.newValue = newValue;
         this.module = module;
-    }
+    }*/
 
     @Override
     public boolean execute() {
@@ -34,13 +36,17 @@ public class EditItem<T> implements Command {
         if(oldValue instanceof Module){
             MainWindow.update(((Module) oldValue).getName(),(Module) newValue);
         }
+        if(oldValue instanceof Link){
+            MainWindow.updateLinkView((Link)oldValue,(Link)newValue);
+        }
+
 
         return true;
     }
 
     @Override
     public boolean reverse() {
-        EditItem<T> reverse = new EditItem(newValue, oldValue, module);
+        EditItem<T> reverse = new EditItem(newValue, oldestValue);
         return reverse.execute();
     }
 }
